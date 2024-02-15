525e6cf25bd08efddc6201e0a428eb4ba3fcd7be
Mike Sazonov <mikhailsazon@gmail.com>
2024-02-15 22:33:14 +0300

    create start menu

--- socks/socks_page/views.py
+++ socks/socks_page/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 from .models import Socks
-from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 # Create your views here.
+menu = {
+    '/socks': 'Каталог продукции',
+    '/about': 'О нас',
+    '/delivery': 'Доставка',
+    '/Contacts': 'Контакты'
+}
 
 
-class StartPage(View):
-    def get(self, request):
-        return render(request, 'socks_page/new.html')
+class StartPage(TemplateView):
+    template_name = 'socks_page/start.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        return context
 
 
-class SocksView(View):
-    def get(self, request):
-        socks = Socks.objects.all()
-        return render(request, 'socks_page/socks.html', {'socks': socks})
+class SocksView(ListView):
+    template_name = 'socks_page/socks.html'
+    model = Socks
+    context_object_name = 'socks'
