03381e77bb71c7e01d8c1a8537218a27a2a68d4b
Mike Sazonov <mikhailsazon@gmail.com>
2024-08-26 17:04:35 +0300

    settings for docker-compose

--- socks/settings.py
+++ socks/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DEBUG"))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '192.168.0.21']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
