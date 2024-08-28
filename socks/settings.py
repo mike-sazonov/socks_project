c4d302e609136327d74efdde0744ae29f361d013
Mike Sazonov <mikhailsazon@gmail.com>
2024-08-28 16:51:28 +0300

    settings.py

--- socks/settings.py
+++ socks/settings.py
@@ -129,6 +129,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = BASE_DIR / '/media/'
+MEDIA_ROOT = BASE_DIR / '/app/media/'
 
-MEDIA_URL = '/usr/scr/app/media/'
+MEDIA_URL = '/app/media/'
